e7d3945da0df3412b16fde7f6dadd998f3d2a188
unknown <cristian>
2023-02-20 12:50:16 -0300

    first commit

---
+++ porfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Project
+
+
+# Create your views here.
+def portafolio(request):
+    projects = Project.objects.all()
+    return render(request, "porfolio/portafolio.html", {"projects": projects})
